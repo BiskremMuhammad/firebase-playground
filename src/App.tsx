66cb20ccd1c988cd739721508e999d39db42be8b
Muhammad Omran <muhammad@wesrom.com>
2022-02-20 20:17:06 +0200

    update test home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const secret = process.env.TEST_KEY;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1 style={{ textAlign: "center" }}>Welcome to my Demo App</h1>
+        <p>{secret}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://biskremer.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Biskremer
         </a>
       </header>
     </div>
